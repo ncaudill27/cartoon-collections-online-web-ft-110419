f03115d0d34800008e80479d4e6b533a4974bb0c
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-07 21:49:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteers)
   planeteers.map { |planeteer| "#{planeteer.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  return true if calls.any?{ |call| call.size > 4 }
 end
 
 def find_the_cheese# code an argument here
