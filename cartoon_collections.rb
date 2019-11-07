31157cbfdf40e23a34ef02c2ecb59940302875cf
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-07 21:54:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.any?{ |call| call.size > 4 ? true : false }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find { |cheese| cheese_types.include?(cheese) }
 end
